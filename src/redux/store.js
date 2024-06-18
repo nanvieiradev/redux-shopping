fea377107c57e44cf0489fe7e08b348329751052
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2024-06-18 20:35:08 -0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,14 @@
-import { createStore, applyMiddleware } from "redux"
-import logger from "redux-logger"
+import {
+    createStore,
+    // applyMiddleware
+} from "redux"
+// import logger from "redux-logger"
 
 import rootReducer from './root-reducer.js'
 
-const store = createStore(rootReducer, applyMiddleware(logger))
+const store = createStore(
+    rootReducer,
+    // applyMiddleware(logger)
+)
 
 export default store
